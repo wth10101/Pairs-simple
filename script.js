92e1c6b0bc1e10604e8a007b5f62b233760ba81a
Wayne Hamilton <wth101@users.noreply.github.com>
2022-11-19 13:50:48 +0000

    Added reset and css clean up

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 const gameGrid = document.getElementById('game')
-
+const fruits = ["apple1","orange1","bannana1","kiwi1","apple2","orange2","bannana2","kiwi2"];
 let numCardsTurned = 0
 let firstCardTurned
 let score = 0
+loadCards()
 
-
-const fruits = ["apple1","orange1","bannana1","kiwi1","apple2","orange2","bannana2","kiwi2"];
-let arr=[0,1,2,3,4,5,6,7]
-arr.sort(() => Math.random() - 0.5);
-for (let i = 0; i<8; i++){
-	const card = document.createElement('img')
-	card.id = fruits[arr[i]]
-	card.classList.add("card")
-	card.value = fruits[arr[i]].substring(0,fruits[arr[i]].length-1)
-	card.src = "img\\blank.jpg"
-	card.addEventListener('click', () => handleClick(card.value, card.id))
-	gameGrid.appendChild(card)
+function loadCards(){
+	let arr=[0,1,2,3,4,5,6,7]
+	arr.sort(() => Math.random() - 0.5);
+	for (let i = 0; i<8; i++){
+		const card = document.createElement('img')
+		card.id = fruits[arr[i]]
+		card.classList.add("card")
+		card.value = fruits[arr[i]].substring(0,fruits[arr[i]].length-1)
+		card.src = "img\\blank.jpg"
+		card.addEventListener('click', () => handleClick(card.value, card.id))
+		gameGrid.appendChild(card)
+	}
 }
 
 function handleClick(value, id){
@@ -29,6 +30,7 @@ function handleClick(value, id){
 		}
 	}else{
 		el.src = 'img\\blank.jpg'
+		el.style.height = '100px';
 		numCardsTurned--
 	}
 	if (numCardsTurned == 2){
@@ -45,7 +47,7 @@ function handleClick(value, id){
 		numCardsTurned = 0
 		if (score == 4){
 			clearInterval(timerVar)
-			setTimeout(() => { //pause to let the card turn before alert
+			setTimeout(() => { //pause so card turns before alert
   			alert(document.getElementById("timer").innerHTML)
 			}, 100)	
 		}
@@ -59,8 +61,6 @@ function countTimer() {
            var hour = Math.floor(totalSeconds /3600);
            var minute = Math.floor((totalSeconds - hour*3600)/60);
            var seconds = totalSeconds - (hour*3600 + minute*60);
-           if(hour < 10)
-             hour = "0"+hour;
            if(minute < 10)
              minute = "0"+minute;
            if(seconds < 10)
@@ -68,3 +68,15 @@ function countTimer() {
            document.getElementById("timer").innerHTML = minute + ":" + seconds;
         }
 
+var el = document.getElementById("reset-btn");
+el.addEventListener('click', () => {
+	const elements = document.getElementsByClassName("card");
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+	score = 0
+	totalSeconds = 0;
+	clearInterval(timerVar)
+	timerVar = setInterval(countTimer, 1000);
+	loadCards()
+})
